5269928b022ffe387c767dff31a3c61b70fa537e
Nutthawara49 <141292245+Nutthawara49@users.noreply.github.com>
2026-03-06 11:28:58 +0700

    chore[fixapiresponse] : handle list in apiresponse

--- lib/core/utils/api_response_parser.dart
+++ lib/core/utils/api_response_parser.dart
@@ -12,14 +12,34 @@ class ApiResponseParser {
   }
 
   static List<T> parseList<T>(
-    dynamic response,
-    T Function(Map<String, dynamic>) fromJson,
-  ) {
-    final data = response is Map ? response['data'] : null;
-    if (data is! List) return [];
-    return data
+  dynamic response,
+  T Function(Map<String, dynamic>) fromJson,
+) {
+
+  if (response is List) {
+    return response
         .whereType<Map<String, dynamic>>()
         .map(fromJson)
         .toList();
+  }
+
+  if (response is Map) {
+    final data = response['data'];
+    if (data is List) {
+      return data
+          .whereType<Map<String, dynamic>>()
+          .map(fromJson)
+          .toList();
+    }
+  }
+
+  return [];
+}
+
+  static bool parseSuccess(dynamic response) {
+    if (response is Map) {
+      return response['success'] == true;
+    }
+    return false;
   }
 }
